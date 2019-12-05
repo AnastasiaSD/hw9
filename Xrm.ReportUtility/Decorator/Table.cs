976d67ddd769856bd1629c7a5c2cb80630bf08c8
AnastasiaSD <32881045+AnastasiaSD@users.noreply.github.com>
2019-12-05 23:04:30 +0500

    Update Table.cs

--- Xrm.ReportUtility/Decorator/Table.cs
+++ Xrm.ReportUtility/Decorator/Table.cs
@@ -1,11 +1,11 @@
 ﻿namespace Xrm.ReportUtility
 {
-    public class Table : ITable
+    public class Table : ITable // таблица, шапку которой собираем
     {
         public string HeaderRow{ get; set; }
         public string RowTemplate{ get; set; }
 
-        public override string ToString()
+        public override string ToString() // выводим полученную шапку
         {
             return HeaderRow;
         }
